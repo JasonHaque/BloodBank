6316f98a642caf5c593eca56d24403d132d26edb
JasonHaque <zahinjason220434@gmail.com>
2019-12-24 00:28:44 +0600

    Added login activity

---
+++ app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bloodbank.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bloodbank.R
+
+class LogInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
--- app/src/main/java/com/example/bloodbank/Activities/MainActivity.kt
+++ app/src/main/java/com/example/bloodbank/Activities/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.example.bloodbank
+package com.example.bloodbank.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bloodbank.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivity(Intent(this,LogInActivity::class.java))
     }
 }
