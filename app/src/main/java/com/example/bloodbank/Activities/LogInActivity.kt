6c970a5c45caf820553f90ebf5abea7ba31c572f
JasonHaque <zahinjason220434@gmail.com>
2019-12-24 16:32:49 +0600

    field validators added

--- app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
+++ app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
@@ -3,6 +3,7 @@ package com.example.bloodbank.Activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.bloodbank.R
 import kotlinx.android.synthetic.main.activity_log_in.*
 
@@ -18,5 +19,18 @@ class LogInActivity : AppCompatActivity() {
             val intent = Intent(this,SignUpActivity::class.java)
             startActivity(intent)
         }
+        login_button.setOnClickListener {
+            val error =validateInputs()
+            if(error){
+                Toast.makeText(this,"Fill up the fields properly",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+    }
+    fun validateInputs():Boolean{
+        if(login_mail.text.toString().isEmpty() || login_password.text.toString().isEmpty()){
+            return true
+        }
+        return false
     }
 }
