6603d6d980443121f94b4647b72d201dba186ef5
JasonHaque <zahinjason220434@gmail.com>
2019-12-24 13:06:05 +0600

    Added new views

---
+++ app/src/main/java/com/example/bloodbank/Views/ProfileView.kt
@@ -0,0 +1,13 @@
+package com.example.bloodbank.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bloodbank.R
+
+class ProfileView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_view)
+    }
+}
---
+++ app/src/main/java/com/example/bloodbank/Views/TImelineView.kt
@@ -0,0 +1,13 @@
+package com.example.bloodbank.Views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bloodbank.R
+
+class TImelineView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline_view)
+    }
+}
