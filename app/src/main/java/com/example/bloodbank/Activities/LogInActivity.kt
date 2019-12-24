b46e2742a107565aa76b035d957018ae530c51dd
JasonHaque <zahinjason220434@gmail.com>
2019-12-24 15:43:10 +0600

    added new elements

--- app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
+++ app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
@@ -1,13 +1,22 @@
 package com.example.bloodbank.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bloodbank.R
+import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        bindListeners()
+    }
+    fun bindListeners(){
+        go_to_signup.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
