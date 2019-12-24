29f1af515b81c8b14c40c465dd7b6fdbe972188f
JasonHaque <zahinjason220434@gmail.com>
2019-12-24 14:12:05 +0600

    added new activity

---
+++ app/src/main/java/com/example/bloodbank/Activities/AdditionalData.kt
@@ -0,0 +1,13 @@
+package com.example.bloodbank.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bloodbank.R
+
+class AdditionalData : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additional_data)
+    }
+}
