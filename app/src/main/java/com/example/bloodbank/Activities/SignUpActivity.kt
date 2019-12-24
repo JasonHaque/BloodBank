817590ec78ceb1ce86399ba93146faf58b7ecc81
JasonHaque <zahinjason220434@gmail.com>
2019-12-24 15:46:35 +0600

    new routes added

--- app/src/main/java/com/example/bloodbank/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/bloodbank/Activities/SignUpActivity.kt
@@ -1,13 +1,22 @@
 package com.example.bloodbank.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bloodbank.R
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        bindWidgets()
+    }
+    fun bindWidgets(){
+        go_to_login.setOnClickListener {
+            val intent = Intent(this,LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
