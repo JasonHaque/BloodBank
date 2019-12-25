4da0af2b5032e04c838f201c1fa44ca31a05ed55
JasonHaque <zahinjason220434@gmail.com>
2019-12-25 20:30:21 +0600

    login method added

--- app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
+++ app/src/main/java/com/example/bloodbank/Activities/LogInActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.bloodbank.R
+import com.example.bloodbank.Views.ProfileView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogInActivity : AppCompatActivity() {
@@ -25,6 +27,14 @@ class LogInActivity : AppCompatActivity() {
                 Toast.makeText(this,"Fill up the fields properly",Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+            val mail = login_mail.text.toString()
+            val pass = login_password.text.toString()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(mail,pass).addOnSuccessListener {
+                Toast.makeText(this,"Login Success",Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,ProfileView::class.java)
+                startActivity(intent)
+            }
+
         }
     }
     fun validateInputs():Boolean{
