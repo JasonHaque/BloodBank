66d97c61ec7b8e316b549a00c8590ea5d36cd1cf
JasonHaque <zahinjason220434@gmail.com>
2019-12-26 18:50:51 +0600

    added new button routes

--- app/src/main/java/com/example/bloodbank/Views/ProfileView.kt
+++ app/src/main/java/com/example/bloodbank/Views/ProfileView.kt
@@ -1,13 +1,26 @@
 package com.example.bloodbank.Views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bloodbank.Activities.LogInActivity
 import com.example.bloodbank.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_profile_view.*
 
 class ProfileView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_view)
+        bindListeners()
+    }
+
+    fun bindListeners(){
+        log_out.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this,LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
