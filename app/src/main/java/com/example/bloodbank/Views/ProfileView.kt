c5c024f5eb3d68c48bbfd1e83cd0d1fb15058859
JasonHaque <zahinjason220434@gmail.com>
2019-12-26 19:10:21 +0600

    added db capabilities

--- app/src/main/java/com/example/bloodbank/Views/ProfileView.kt
+++ app/src/main/java/com/example/bloodbank/Views/ProfileView.kt
@@ -6,14 +6,59 @@ import android.os.Bundle
 import com.example.bloodbank.Activities.LogInActivity
 import com.example.bloodbank.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_profile_view.*
 
 class ProfileView : AppCompatActivity() {
 
+    private var db = FirebaseDatabase.getInstance().getReference()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_view)
+        bindWidgets()
         bindListeners()
+    }
+
+    fun bindWidgets(){
+        db.child("Users").child(FirebaseAuth.getInstance().currentUser?.uid.toString())
+            .child("Data").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    println("Error receiving data")
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+
+                    val children = p0!!.children
+                    var name =""
+                    children.forEach{
+
+                        if(it.key.equals("firstName")){
+                            name = it.value.toString()
+                        }
+                        else if(it.key.equals("lastName")){
+                            name+=it.value.toString()
+                        }
+                        else if(it.key.equals("age")){
+                            user_age.text=it.value.toString()
+                        }
+                        else if(it.key.equals("contact")){
+                            user_contact.text=it.value.toString()
+
+                        }
+                        else if(it.key.equals("bloodGroup")){
+                            user_blood_group.text=it.value.toString()
+
+                        }
+                    }
+                    user_name.text=name
+                    user_mail.text=FirebaseAuth.getInstance().currentUser?.email
+
+                }
+
+            })
     }
 
     fun bindListeners(){
