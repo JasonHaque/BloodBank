f8d063177421e7cfab12a846092950bb10aa7052
JasonHaque <zahinjason220434@gmail.com>
2019-12-26 13:02:59 +0600

    added db capabilities

--- app/src/main/java/com/example/bloodbank/Activities/AdditionalData.kt
+++ app/src/main/java/com/example/bloodbank/Activities/AdditionalData.kt
@@ -1,13 +1,19 @@
 package com.example.bloodbank.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import android.widget.Toast
+import com.example.bloodbank.Classes.UserData
 import com.example.bloodbank.R
+import com.example.bloodbank.Views.ProfileView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_additional_data.*
 
 class AdditionalData : AppCompatActivity() {
-
+    private var db = FirebaseDatabase.getInstance().reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_additional_data)
@@ -24,6 +30,14 @@ class AdditionalData : AppCompatActivity() {
                 Toast.makeText(this,error,Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+            val bg = findViewById<RadioButton>(bloodGroup.checkedRadioButtonId).text.toString()
+            print(bg)
+            val data:UserData = UserData(firstName,lastName,ageData.toInt(),contactData,bg)
+            db.child("Users").child(FirebaseAuth.getInstance().currentUser?.uid.toString())
+                .child("Data").setValue(data).addOnSuccessListener {
+                    Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,ProfileView::class.java))
+                }
         }
     }
 
