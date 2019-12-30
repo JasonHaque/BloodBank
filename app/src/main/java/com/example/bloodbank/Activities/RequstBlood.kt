bee1b8ec38e23d31b6a9d84db7d2415553e4bb89
JasonHaque <zahinjason220434@gmail.com>
2019-12-30 15:59:58 +0600

    added new activity

---
+++ app/src/main/java/com/example/bloodbank/Activities/RequstBlood.kt
@@ -0,0 +1,13 @@
+package com.example.bloodbank.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bloodbank.R
+
+class RequstBlood : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_requst_blood)
+    }
+}
