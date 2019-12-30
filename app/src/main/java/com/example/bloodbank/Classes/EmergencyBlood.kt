4a0f0745782ca22c69cc63aecd862e4dbd942be9
JasonHaque <zahinjason220434@gmail.com>
2019-12-30 16:37:25 +0600

    added new data class

---
+++ app/src/main/java/com/example/bloodbank/Classes/EmergencyBlood.kt
@@ -0,0 +1,8 @@
+package com.example.bloodbank.Classes
+
+data class EmergencyBlood(
+    val bloodgroup: String = "",
+    val location: String = "",
+    val time: String = "",
+    val amount: String = ""
+)
