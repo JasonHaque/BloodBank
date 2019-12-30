0de5c17a706f13c8848f345cec683100df36453a
JasonHaque <zahinjason220434@gmail.com>
2019-12-30 16:17:36 +0600

    code cleaned

--- app/src/main/java/com/example/bloodbank/Activities/RequstBlood.kt
+++ app/src/main/java/com/example/bloodbank/Activities/RequstBlood.kt
@@ -3,11 +3,18 @@ package com.example.bloodbank.Activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bloodbank.R
+import kotlinx.android.synthetic.main.activity_requst_blood.*
 
 class RequstBlood : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_requst_blood)
+        bindListeners()
+    }
+    fun bindListeners(){
+        submit_request.setOnClickListener {
+            //TODO: add a new listener
+        }
     }
 }
