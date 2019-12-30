01de9d21b42c5870670bd21fba62fe6c012ec24d
JasonHaque <zahinjason220434@gmail.com>
2019-12-30 15:58:47 +0600

    added new listeners

--- app/src/main/java/com/example/bloodbank/Views/TImelineView.kt
+++ app/src/main/java/com/example/bloodbank/Views/TImelineView.kt
@@ -3,11 +3,19 @@ package com.example.bloodbank.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bloodbank.R
+import kotlinx.android.synthetic.main.activity_timeline_view.*
 
 class TImelineView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timeline_view)
+        bindListeners()
+    }
+
+    fun bindListeners(){
+        Request_blood.setOnClickListener {
+            //TODO: add a new listener
+        }
     }
 }
