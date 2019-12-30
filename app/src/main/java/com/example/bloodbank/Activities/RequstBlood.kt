2e5fed9572d0bcadbb75d56edc22d6fb404a0941
JasonHaque <zahinjason220434@gmail.com>
2019-12-30 16:27:24 +0600

    added new validators

--- app/src/main/java/com/example/bloodbank/Activities/RequstBlood.kt
+++ app/src/main/java/com/example/bloodbank/Activities/RequstBlood.kt
@@ -2,6 +2,7 @@ package com.example.bloodbank.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.bloodbank.R
 import kotlinx.android.synthetic.main.activity_requst_blood.*
 
@@ -14,7 +15,23 @@ class RequstBlood : AppCompatActivity() {
     }
     private fun bindListeners(){
         submit_request.setOnClickListener {
-            //TODO: add a new listener
+            val error =validateInputs()
+            if (error != ""){
+                Toast.makeText(this,error,Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
         }
+    }
+    private fun validateInputs():String{
+        val bloodgroup = requesting_blood_group.text.toString()
+        val location = requesting_location.text.toString()
+        val time = requesting_time.text.toString()
+        val amount = amount_of_blood.text.toString()
+
+        if (bloodgroup.isEmpty() || location.isEmpty() || time.isEmpty() || amount.isEmpty()){
+            return "Fill up the fields"
+        }
+
+        return ""
     }
 }
